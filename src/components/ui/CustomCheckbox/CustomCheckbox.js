2bb340a49f2d82074367139291652544ddd18b2f
Oly Erofeeva <o.leland@yandex.ru>
2021-04-17 16:21:25 +0300

    ui kit: custom checkbox

---
+++ src/components/ui/CustomCheckbox/CustomCheckbox.js
@@ -0,0 +1,37 @@
+import styles from './CustomCheckbox.module.scss'
+import { useState } from 'react'
+import * as uuid from 'uuid'
+
+// TODO: disabled state styles
+// TODO: click handler
+
+const CustomCheckbox = ({ labelText = '', classNameExt = '' }) => {
+  const [checkboxId] = useState(uuid.v4())
+  const [isChecked, setIsChecked] = useState(false)
+
+  const clickHandler = () => {
+    setIsChecked(!isChecked)
+  }
+
+  return (
+    <label className={`${styles.label} ${classNameExt}`} htmlFor={checkboxId} onClick={clickHandler}>
+      <input className={styles.checkboxHidden} type="checkbox" />
+
+      <svg className={styles.checkboxVisible} id={checkboxId} width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <rect className={styles.box} x="0.5" y="0.5" width="19" height="19" rx="3.5" stroke="#ddd" />
+
+        {isChecked && (
+          <path
+            className={styles.check_accent}
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M6.96826 13.4452L4.13989 10.6169L5.55411 9.20266L8.38248 12.031L15.4535 4.95996L16.8678 6.37417L9.79669 13.4452L9.79675 13.4453L8.38253 14.8595L8.38248 14.8595L6.96826 13.4452Z"
+          />
+        )}
+      </svg>
+      {labelText}
+    </label>
+  )
+}
+
+export default CustomCheckbox
