ab9b05337e1a7b9873bb9ab625b251dddc1865a5
Oly Erofeeva <o.leland@yandex.ru>
2021-04-18 23:29:15 +0300

    refactor: header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,27 @@ import styles from './Header.module.scss'
 import { useMediaQuery } from 'react-responsive'
 
 import ProductSearch from '../ProductSearch/ProductSearch'
+import FilterButton from '../ui/FilterButton/FilterButton'
 
-const Header = () => {
+const Header = ({ onFilterButtonClick, filter, updateFilter, productsReqStatus }) => {
   const isMobile = useMediaQuery({ query: 'screen and (max-width: 414px)' })
 
   return (
     <header className={styles.root}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Products</h1>
-        {!isMobile && <ProductSearch />}
+      <div className={styles.mainPanel}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Products</h1>
+          {!isMobile && (
+            <ProductSearch filter={filter} updateFilter={updateFilter} productsReqStatus={productsReqStatus} />
+          )}
+        </div>
       </div>
+      {isMobile && (
+        <div className={styles.additionalPanel}>
+          <ProductSearch filter={filter} updateFilter={updateFilter} productsReqStatus={productsReqStatus} />
+          <FilterButton classNameExt={styles.filtersButton} onClick={onFilterButtonClick} />
+        </div>
+      )}
     </header>
   )
 }
