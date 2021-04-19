05f375aa631248a8ec3977e2a49a18f3a1423972
Oly Erofeeva <o.leland@yandex.ru>
2021-04-19 17:48:01 +0300

    fix: product card wrapped in <a>, has styles for hover & focus

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -11,22 +11,24 @@ const ProductCard = ({
   isLimited,
 }) => {
   return (
-    <article className={styles.root}>
-      <img className={styles.image} src={imageSrc} alt={categoryName} />
-      <div className={styles.infoContainer}>
-        <p className={styles.category}>{categoryName}</p>
-        <h3 className={styles.title}>{productName}</h3>
-        <p className={styles.description}>{productDescription}</p>
-        <div className={styles.priceContainer}>
-          <p className={styles.price}>${price}</p>
-          {discount && <p className={styles.discount}>Discount ${discount} per bag</p>}
+    <a className={styles.root} href="/" target="_self">
+      <article className={styles.content}>
+        <img className={styles.image} src={imageSrc} alt={categoryName} />
+        <div className={styles.infoContainer}>
+          <p className={styles.category}>{categoryName}</p>
+          <h3 className={styles.title}>{productName}</h3>
+          <p className={styles.description}>{productDescription}</p>
+          <div className={styles.priceContainer}>
+            <p className={styles.price}>${price}</p>
+            {discount && <p className={styles.discount}>Discount ${discount} per bag</p>}
+          </div>
         </div>
-      </div>
-      <ul className={styles.statusBadges}>
-        {isNew && <li className={`${styles.statusBadge} ${styles.statusBadge_new}`}>New</li>}
-        {isLimited && <li className={`${styles.statusBadge} ${styles.statusBadge_limited}`}>Limited</li>}
-      </ul>
-    </article>
+        <ul className={styles.statusBadges}>
+          {isNew && <li className={`${styles.statusBadge} ${styles.statusBadge_new}`}>New</li>}
+          {isLimited && <li className={`${styles.statusBadge} ${styles.statusBadge_limited}`}>Limited</li>}
+        </ul>
+      </article>
+    </a>
   )
 }
 
